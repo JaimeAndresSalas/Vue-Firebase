7c9000181ee6e8afd53b6c1e43cca17193e03d8e
Jaime Andres Salas <jsalasmoreira@gmail.com>
2020-04-26 18:19:26 -0500

    Finalizacion micro proyecto

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -2,15 +2,17 @@
   <div>
     <h1>Lista de tareas</h1>
     <router-link :to="{name: 'agregar'}">
-      <button>Agregar</button>
+      <button class="btn btn-success btn-block">Agregar</button>
     </router-link>
-    <ul>
-      <li v-for="item of tareas" :key="item.id">
-        {{item.id}} - {{item.Tareas}}
-        <router-link :to="{name:'editar', params:{id: item.id}}">
-          <button @click="getTareas(id)">Editar</button>
-        </router-link>
-        <button @click="eliminarTarea(item.id)">Eliminar</button>
+    <ul class="list-group mt-5">
+      <li v-for="item of tareas" :key="item.id" class="list-group-item">
+        {{item.id}} - {{item.nombre}}
+        <div class="float-right">
+          <router-link :to="{name:'editar', params:{id: item.id}}">
+            <button class="btn btn-warning btn-sm mr-2" @click="getTareas(id)">Editar</button>
+          </router-link>
+          <button @click="eliminarTarea(item.id)" class="btn btn-danger btn-sm">Eliminar</button>
+        </div>
       </li>
     </ul>
   </div>
